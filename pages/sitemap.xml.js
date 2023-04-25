51ac8d113c91bb5793f6d3ff24d35d23956633ce
tuan.ly <tuan.ly@9thwonder.com>
2023-04-25 15:51:35 +0700

    update frontend URL dev

--- pages/sitemap.xml.js
+++ pages/sitemap.xml.js
@@ -5,7 +5,7 @@ export default function Sitemap() {}
 export function getServerSideProps(ctx) {
   return getSitemapProps(ctx, {
     // sitemapIndexPath: '/sitemap_index.xml',
-    frontendUrl: process.env.FRONTEND_URL,
+    frontendUrl: process.env.FRONTEND_URL || 'https://hnn38av7e7w8o4to8pn2eny2f.js.wpenginepowered.com/',
     // frontendUrl: 'https://hnn38av7e7w8o4to8pn2eny2f.js.wpenginepowered.com/',
     // frontendUrl: 'http://localhost:3000/',
     pages: [
