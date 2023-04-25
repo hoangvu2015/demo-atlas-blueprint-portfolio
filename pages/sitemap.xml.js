a32db305a7c5b4b59dae398161e8d3b0453b9d9e
tuan.ly <tuan.ly@9thwonder.com>
2023-04-25 15:35:02 +0700

    test sitemap

---
+++ pages/sitemap.xml.js
@@ -0,0 +1,17 @@
+import { getSitemapProps } from '@faustwp/core';
+
+export default function Sitemap() {}
+
+export function getServerSideProps(ctx) {
+  return getSitemapProps(ctx, {
+    // sitemapIndexPath: '/sitemap_index.xml',
+    frontendUrl: process.env.FRONTEND_URL,
+    // frontendUrl: 'https://hnn38av7e7w8o4to8pn2eny2f.js.wpenginepowered.com/',
+    // frontendUrl: 'http://localhost:3000/',
+    pages: [
+      {
+        path: '/hard-code-page',
+      },
+    ],
+  });
+}
