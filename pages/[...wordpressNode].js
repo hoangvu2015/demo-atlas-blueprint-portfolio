428813bc3241fd98457b22eff9d5caeee6debb3a
Vu Vo 9thwonder <hoangvu2015@users.noreply.github.com>
2023-06-27 14:54:37 +0700

    Update [...wordpressNode].js

--- pages/[...wordpressNode].js
+++ pages/[...wordpressNode].js
@@ -1,6 +1,6 @@
 import { getWordPressProps, WordPressTemplate } from '@faustwp/core';
 // import { WordPressTemplate } from '@faustwp/core';
-// import { getNextServerSideProps } from '@faustwp/core/dist/cjs/getProps';
+import { getNextServerSideProps } from '@faustwp/core/dist/cjs/getProps';
 
 export default function Page(props) {
   // console.log(props, 'props WordPressTemplate');
@@ -17,9 +17,9 @@ export async function getServerSideProps(ctx) {
 //   return getWordPressProps({ ctx, revalidate: 10});
 // }
 
-export async function getStaticPaths() {
-  return {
-    paths: [],
-    fallback: 'blocking',
-  };
-}
+// export async function getStaticPaths() {
+//   return {
+//     paths: [],
+//     fallback: 'blocking',
+//   };
+// }
